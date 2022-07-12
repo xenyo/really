43ee63ae6bd58db74249e501eb594b9a3e2b58b8
Parkle Lee <parkle.lee@xenyo.com>
2022-07-12 14:05:56 +0800

    Add properties

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ const fs = require("fs");
 const properties = require("./properties.yml");
 const globalValues = require("./global-values.yml");
 
-const escape = str => str.replace(/([^0-9a-zA-Z_-])/, "\\$1");
-
 const css = Object.entries(properties).map(([property, values]) => {
-  return values.concat(globalValues).map(value => {
-    return [
-      `.${property}-${escape(value)} { ${property}: ${value}; }`,
-      `.${property}-${escape(value)}\\! { ${property}: ${value} !important; }`,
-    ].join("\n");
+  return values
+    // .concat(globalValues)
+    .map(value => {
+      const classValue = value.toString()
+        .replace(" ", "-")
+        .replace(/([^0-9a-zA-Z_-])/, "\\$1");
+      return [
+        `.${property}-${classValue} { ${property}: ${value}; }`,
+        `.${property}-${classValue}\\! { ${property}: ${value} !important; }`,
+      ].join("\n");
+    }).join("\n");
   }).join("\n");
-}).join("\n");
 
 fs.writeFileSync("dist/generator/literally.css", css);
